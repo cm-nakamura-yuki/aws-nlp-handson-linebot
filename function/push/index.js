b6216fcf13cf8b05d0856bdfbc95e0ec3ef18db9
nakamura.yuki <>
2020-03-18 00:54:01 +0900

    Modified to send push message using S3 put event

--- function/push/index.js
+++ function/push/index.js
@@ -1,10 +1,36 @@
 const Line = require('@line/bot-sdk');
+const Aws = require('aws-sdk');
 const channelAccessToken = process.env.CHANNEL_ACCESS_TOKEN;
 const client = new Line.Client({channelAccessToken: channelAccessToken});
+const s3 = new Aws.S3();
 
 exports.handler = async(event) => {
     console.log(JSON.stringify(event));
 
+    for (let i = 0; i < event.Records.length; i++) {
+        let key = event.Records[i].s3.object.key;
+        let param = {
+            Key: key,
+            Bucket: event.Records[i].s3.bucket.name
+        };
+        let result = await s3.getObject(param).promise();
+        console.log(JSON.stringify(result));
 
-    return { statusCode: 200, body: 'OK'};
+        let body = JSON.parse(result.Body.toString());
+
+        let cutPos = key.indexOf('_');
+        let userId = key.substring(cutPos, -1);
+
+        let message = [{
+            type: 'text',
+            text: 'Amazon Transcribeの解析結果です。'
+        },{
+            type: 'text',
+            text: body.results.transcripts[0].transcript
+        }];
+
+        await client.pushMessage(userId, message);
+    }
+
+    return { statusCode: 200, body: 'OK' };
 };
